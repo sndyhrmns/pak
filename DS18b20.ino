4ad2ab1fca35bf5412d71f22de5d17117c62b94c
sndyhrmns <sndyhrmns@gmail.com>
2022-04-22 08:53:09 +0800

    fc

---
+++ DS18b20.ino
@@ -0,0 +1,22 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+#define ONE_WIRE_BUS 2 // DATA Kabel kunin9 D4
+OneWire oneWire(ONE_WIRE_BUS); // deklarasi library OneWire dengan Variabel oneWire bernilai ONE_WIRE_BUS
+DallasTemperature sensors(&oneWire); 
+
+
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+sensors.requestTemperatures();
+Serial.println(sensors.getTempCByIndex(0));
+}
