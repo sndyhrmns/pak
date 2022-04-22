990bba843a062573fea955ae21befd8df8853ff2
sndyhrmns <sndyhrmns@gmail.com>
2022-04-22 13:50:39 +0800

    dirapihkan

--- arduino/DS18b20.ino
+++ arduino/DS18b20.ino
@@ -1,22 +1,15 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-
-#define ONE_WIRE_BUS 2 // DATA Kabel kunin9 D4
+#define ONE_WIRE_BUS 2 // DATA Kabel kuning D4
 OneWire oneWire(ONE_WIRE_BUS); // deklarasi library OneWire dengan Variabel oneWire bernilai ONE_WIRE_BUS
-DallasTemperature sensors(&oneWire); 
-
-
+DallasTemperature suhu(&oneWire); 
 
 void setup() {
   Serial.begin(9600);
-  sensors.begin();
+  suhu.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-
-sensors.requestTemperatures();
-Serial.println(sensors.getTempCByIndex(0));
+  suhu.requestTemperatures();
+  Serial.println(suhu.getTempCByIndex(0));
 }
